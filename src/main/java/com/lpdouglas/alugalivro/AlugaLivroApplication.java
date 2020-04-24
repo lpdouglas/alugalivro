c9ed199271244d822382271b0cfd848a05fbdaa3
lpdouglas <lpdouglas@live.com>
2020-04-23 23:22:52 -0300

    Criado projeto na linguagem Java e utilizando o framework Spring

---
+++ src/main/java/com/lpdouglas/alugalivro/AlugaLivroApplication.java
@@ -0,0 +1,13 @@
+package com.lpdouglas.alugalivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugaLivroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlugaLivroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lpdouglas/alugalivro/AlugaLivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpdouglas.alugalivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugaLivroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
